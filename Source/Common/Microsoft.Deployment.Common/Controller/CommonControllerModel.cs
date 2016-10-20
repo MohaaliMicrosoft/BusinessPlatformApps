7a153b1c0369d1f3a6394e5d6b169bd9468697f7
MohaaliMicrosoft <mohaali@DESKTOP-TMPRQHU>
2016-10-19 17:43:34 -0700

    Including changes to the common set of properties and moved AppName to the Request

--- Source/Common/Microsoft.Deployment.Common/Controller/CommonControllerModel.cs
+++ Source/Common/Microsoft.Deployment.Common/Controller/CommonControllerModel.cs
@@ -8,11 +8,14 @@ namespace Microsoft.Deployment.Common.Controller
         public AppFactory AppFactory { get; set; }
         public Dictionary<string, string> LoggingParameters { get; set; }
         public string Source { get; set; }
-        public string VirtualPathRoot { get; set; }
+        public string Referer { get; set; }
+
+
         public string AppRootPath { get; set; }
         public string AppPath { get; set; }
         public string SiteCommonPath { get; set; }
         public string WebsiteRootUrl { get; set; }
-        public string Referer { get; set; }
+        public string ServiceRootFilePath { get; set; }
+
     }
 }
