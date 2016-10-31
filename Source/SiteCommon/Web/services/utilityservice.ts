21286f17288a8ede4796ddbecd2d3b91ac3886f9
MohaaliMicrosoft <mohaali@DESKTOP-TMPRQHU>
2016-10-31 11:39:43 -0700

    Made modification to impersonation

--- Source/SiteCommon/Web/services/utilityservice.ts
+++ Source/SiteCommon/Web/services/utilityservice.ts
@@ -101,35 +101,7 @@ export class UtilityService {
         return isValid;
     }
 
-    ValidatePassword(pwd: string, pwd2: string, length: number): string {
-        let passwordError: string = '';
-        if (pwd !== pwd2) {
-            passwordError = 'Passwords do not match.';
-        } else if (length && pwd.length < length) {
-            passwordError = 'Password must be at least eight characters long.';
-        } else if ((/\s/g).test(pwd)) {
-            passwordError = 'Password should not contain spaces.';
-        } else if (!(/[A-Z]/).test(pwd) || (/^[a-zA-Z0-9]*$/).test(pwd)) {
-            passwordError = 'Password must contain at least one uppercase character and at least one special character.';
-        }
-        return passwordError;
-    }
-
-    ValidateUsername(username: string, invalidUsernames: string[], usernameText: string): string {
-        let usernameError: string = '';
-        if ((/\s/g).test(username)) {
-            usernameError = `${usernameText} should not contain spaces.`;
-        } else if (username.length > 63) {
-            usernameError = `${usernameText} must not be longer than 63 characters.`;
-        } else if (invalidUsernames.indexOf(username.toLowerCase()) > -1) {
-            usernameError = `${usernameText} cannot be a reserved SQL system name.`;
-        } else if (!(/^[a-zA-Z0-9\-]+$/).test(username)) {
-            usernameError = `${usernameText} must only contain alphanumeric characters or hyphens.`;
-        } else if (username[0] === '-' || username[username.length - 1] === '-') {
-            usernameError = `${usernameText} must not start or end with a hyphen.`;
-        }
-        return usernameError;
-    }
+   
 
     // Add items to the session storage - should use DataStore where possible
     SaveItem(key, value) {
