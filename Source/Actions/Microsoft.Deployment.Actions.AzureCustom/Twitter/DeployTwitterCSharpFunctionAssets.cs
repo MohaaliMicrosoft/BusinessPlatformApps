e9a7774b1444b59b0c4143d08c207b84e40e945c
mohaalilocal <mohaalilocal@DESKTOP-TMSE9TU>
2016-11-02 16:51:44 -0700

    Added fixes to deploy CSharp function Assets

--- Source/Actions/Microsoft.Deployment.Actions.AzureCustom/Twitter/DeployTwitterCSharpFunctionAssets.cs
+++ Source/Actions/Microsoft.Deployment.Actions.AzureCustom/Twitter/DeployTwitterCSharpFunctionAssets.cs
@@ -74,7 +74,7 @@ namespace Microsoft.Deployment.Actions.AzureCustom.Twitter
 
             var appSettingCreated = await client.ExecuteGenericRequestWithHeaderAsync(HttpMethod.Post, @"https://web1.appsvcux.ext.azure.com/websites/api/Websites/UpdateConfigConnectionStrings",
             JsonUtility.GetJsonStringFromObject(obj));
-            response = appSettingCreated.Content.ReadAsStringAsync();
+            response = await appSettingCreated.Content.ReadAsStringAsync();
             if (!appSettingCreated.IsSuccessStatusCode)
             {
                 return new ActionResponse(ActionStatus.Failure, JsonUtility.GetJObjectFromJsonString(response),
