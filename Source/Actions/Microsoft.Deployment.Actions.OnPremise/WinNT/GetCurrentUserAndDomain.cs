198104bbe53d4fa3fdc17baf58d65194b112c3fd
raymondb <raymondb@microsoft.com>
2016-11-03 14:39:32 -0700

    Simplify some calls. Call WindowsIdentity instead of env variable

--- Source/Actions/Microsoft.Deployment.Actions.OnPremise/WinNT/GetCurrentUserAndDomain.cs
+++ Source/Actions/Microsoft.Deployment.Actions.OnPremise/WinNT/GetCurrentUserAndDomain.cs
@@ -1,25 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Deployment.Common.ActionModel;
-using Microsoft.Deployment.Common.Actions;
-using Microsoft.Deployment.Common.Helpers;
-
-namespace Microsoft.Deployment.Actions.OnPremise.WinNT
+﻿namespace Microsoft.Deployment.Actions.OnPremise.WinNT
 {
+    using Microsoft.Deployment.Common.ActionModel;
+    using Microsoft.Deployment.Common.Actions;
+    using System.ComponentModel.Composition;
+    using System.Security.Principal;
+    using System.Threading.Tasks;
+
     [Export(typeof(IAction))]
     public class GetCurrentUserAndDomain : BaseAction
     {
         public override async Task<ActionResponse> ExecuteActionAsync(ActionRequest request)
         {
-            string domain = Environment.GetEnvironmentVariable("USERDOMAIN");
-            string user = Environment.GetEnvironmentVariable("USERNAME");
-            string domainAccount = $"{NTHelper.CleanDomain(domain)}\\{NTHelper.CleanUsername(user)}";
-
-            return new ActionResponse(ActionStatus.Success, domainAccount);
+            return new ActionResponse(ActionStatus.Success, WindowsIdentity.GetCurrent().Name);
         }
     }
 }
